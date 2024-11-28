37b0ac0db0e7ba2921cac68b5a7172603beeef43
unclebonn <khoita1008@gmail.com>
2024-11-28 14:42:42 +0700

    fix update

--- src/app/auth/sign-in/[[...rest]]/page.tsx
+++ src/app/auth/sign-in/[[...rest]]/page.tsx
@@ -1,26 +1,9 @@
-"use client"
-
 import { SignIn } from '@clerk/nextjs';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
 
 export default function SignInPage() {
-  const params = useSearchParams()
-
-  const router = useRouter()
-
-  const redirectUrl = params.get("redirect_url")
-
-  useEffect(() => {
-    if (redirectUrl) {
-      router.push(redirectUrl)
-    }
-  }, [])
-
-
   return (
     <main className='flex justify-center items-center h-screen bg-login-custom'>
       <SignIn />
     </main>
-  );
+  );  
 }
