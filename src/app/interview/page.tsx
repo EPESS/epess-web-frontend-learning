3b45f74768718fb228392f7b88e699ead992140c
An Vu <hoanganwm@gmail.com>
2024-12-06 21:41:36 +0700

    commit interview

---
+++ src/app/interview/page.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { PageClientImpl } from './components/PageClientImpl';
+import { isVideoCodec } from '@/lib/types';
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    hq?: string;
+    codec?: string;
+  };
+}) {
+  const codec =
+    typeof searchParams.codec === 'string' && isVideoCodec(searchParams.codec)
+      ? searchParams.codec
+      : 'vp9';
+  const hq = searchParams.hq === 'true' ? true : false;
+
+  return <PageClientImpl roomName={'123'} hq={hq} codec={codec} />;
+}
